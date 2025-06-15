fe5fa3fd9accfbc81ed152f2231f5bd145f66e18
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-15 21:16:52 +0700

    Soal 10 logika js: Cek bilangan prima

---
+++ soal-logika-js/soal10.js
@@ -0,0 +1,10 @@
+// Soal 10: Cek Bilangan Prima
+
+function isPrime(n) {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+}
+console.log(isPrime(7)); // true
