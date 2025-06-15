e5bbec0a44581f223f4cdb380597adac30d29ca7
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-15 20:55:07 +0700

    Soal 23: Debug

---
+++ soal23.js
@@ -0,0 +1,9 @@
+// Soal 23: Debug
+// Soal:
+// Apa yang salah dengan kode berikut?
+
+const name = "Ali gordon sanusi";
+
+console.log("Nama saya adalah: " + name.toUpperCase());
+
+// name.toUpper seharusnya name.toUpperCase() karena fungsi.
