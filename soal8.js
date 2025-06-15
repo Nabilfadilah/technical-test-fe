b5f22e51f25fd2195b0e1bd5d011d11bb7ed6453
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-15 20:48:57 +0700

    Soal 8: Function Expression

---
+++ soal8.js
@@ -0,0 +1,6 @@
+// Soal 8: Function Expression
+// Buat function untuk menghitung luas persegi panjang.
+
+const hitungLuas = (panjang, lebar) => panjang * lebar;
+
+console.log(hitungLuas(5, 3)); // 15
