074d709b611fa850b02158d43f42162ea0b11ec7
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-15 20:49:24 +0700

    Soal 10: Ternary Operator

---
+++ soal10.js
@@ -0,0 +1,6 @@
+// Soal 10: Ternary Operator
+// Buat pengecekan nilai: jika nilai >= 75 tampilkan “Lulus”, jika tidak “Tidak Lulus”.
+
+const nilai = 80;
+const status = nilai >= 75 ? "Lulus" : "Tidak Lulus";
+console.log(status); // Lulus
