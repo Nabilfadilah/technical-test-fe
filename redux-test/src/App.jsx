2a28cc349f87443e3b52e658a21851c75a78fd97
Nabilfadilah <mohammadfadilah00@gmail.com>
2025-06-23 10:31:51 +0700

    Menampilkan komponen produk dan keranjang

--- redux-test/src/App.jsx
+++ redux-test/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from "react";
+import "./App.css";
+import Cart from "./components/Cart";
+import Product from "./components/Product";
+
+const dummyProduct = {
+  id: 1,
+  name: "Keyboard",
+  price: 200000,
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1>React Redux Shop</h1>
+        {/* menampilkan produk dan keranjang */}
+        <Product product={dummyProduct} />
+        <Cart />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      {/* <div>
+        <p>Menampilkan Daftar card</p>
+        <Cart />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <div>
+        <p>Tambah item cart</p>
+        <Product />
+      </div> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
